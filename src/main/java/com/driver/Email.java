0bed2c5ce8c2578c482bad4959f7359568fed6d7
utsav <guptautsavkumar126@gmail.com>
2023-03-18 19:46:47 +0530

    First

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,11 +1,14 @@
 package com.driver;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Email {
 
     private String emailId;
     private String password;
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -18,12 +21,68 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
-        // 1. It contains at least 8 characters
-        // 2. It contains at least one uppercase letter
-        // 3. It contains at least one lowercase letter
-        // 4. It contains at least one digit
-        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if (oldPassword == this.password) {
+            // 1. It contains at least 8 characters
+            if (newPassword.length() >= 8 && isUppercase(newPassword) && isLowercase(newPassword) && hasSpecial(newPassword) && hasDigit(newPassword)) {
+                this.password = newPassword;
+            }
+            // 2. It contains at least one uppercase letter
+            // 3. It contains at least one lowercase letter
+            // 4. It contains at least one digit
+            // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        }
+
+    }
+
+    Map<Character,Integer>digitMap=new HashMap<>();
+
+    private boolean isUppercase(String newPassword) {
+        for(int i=0;i<newPassword.length();i++){
+            digitMap.put(newPassword.charAt(i), digitMap.getOrDefault(newPassword.charAt(i),0)+1);
+        }
+        for (Character c:digitMap.keySet()) {
+            if ((int)c >= 65 && (int)c <= 90) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isLowercase(String newPassword) {
+        for(int i=0;i<newPassword.length();i++){
+            digitMap.put(newPassword.charAt(i), digitMap.getOrDefault(newPassword.charAt(i),0)+1);
+        }
+        for (Character c:digitMap.keySet()) {
+            if ((int)c >= 97 && (int)c <= 122) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasSpecial(String newPassword) {
+        for(int i=0;i<newPassword.length();i++){
+            digitMap.put(newPassword.charAt(i), digitMap.getOrDefault(newPassword.charAt(i),0)+1);
+        }
+        for(Character c : digitMap.keySet()){
+            if(c.equals('@')||c.equals('#')||c.equals('$')||c.equals("%")||c.equals('&')||c.equals('*')||c.equals('?')||c.equals('+')){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasDigit(String newPassword) {
+        for(int i=0;i<newPassword.length();i++){
+            digitMap.put(newPassword.charAt(i), digitMap.getOrDefault(newPassword.charAt(i),0)+1);
+        }
+        for (Character c:digitMap.keySet()) {
+            if ((int)c >= 48 && (int)c <= 57) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -6,17 +6,20 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Workspace extends Gmail{
+public class  Workspace extends Gmail{
 
     private ArrayList<Meeting> calendar; // Stores all the meetings
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
+        super(emailId,Integer.MAX_VALUE);
+        calendar=new ArrayList<>();
 
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
+        calendar.add(meeting);
 
     }
 
